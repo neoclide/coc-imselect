ae248e744ac3c092c6d957d3923595676666b0e2
chemzqm <chemzqm@gmail.com>
2019-07-29 16:05:26 +0800

    ignore lib code

--- lib/util.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Color {
-    red: string;
-    green: string;
-    blue: string;
-}
-export declare function setColor(red: string, green: string, blue: string): Promise<void>;
-export declare function getColor(): Promise<Color>;
-export declare function selectInput(method: string): Promise<void>;
